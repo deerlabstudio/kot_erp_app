face674500edc38d7c8a898abae05c551af53341
Alex Mejicanos <alexmejicanos@outlook.com>
2019-07-24 23:55:55 -0600

    Finish Markup for Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+import '../../styles/Login.css';
 
 class Login extends Component {
   state = {
 
   };
 
+  doLogin = () => {
+    this.props.history.push('/backoffice');
+  }
+
   render() {
     return (
-      <div>Login</div>
+      <div className="container-login">
+        <div className="wrap-login">
+          <img
+            className="logo"
+            src="./statics/images/deerlab_logo.jpg"
+            alt="Deerlab Logo"
+          />
+          <Form className="form-login">
+            <Form.Group>
+              <Form.Control type="email" placeholder="Insert your email" />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Control type="password" placeholder="Insert your password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className="login-button" onClick={this.doLogin}>Login</Button>
+          </Form>
+        </div>
+        <div className="company-footer">
+          <span>
+            Created by
+            <a href="https://deerlab.studio" target="_blank" rel="noopener noreferrer"> Deerlab Studio</a>
+          </span>
+        </div>
+      </div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -56,5 +57,10 @@ module.exports = {
       filename: 'styles/[name].css',
       chunkFilename: '[id].css',
     }),
+    new CopyWebpackPlugin([
+      {
+        from: 'src/statics', to: 'statics',
+      },
+    ]),
   ],
 };
